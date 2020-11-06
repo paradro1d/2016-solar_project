9fd9934cd39315b828f86b7ab4c0dc03b0e42c52
Paradro1d <gorbunov.sm@phystech.edu>
2020-11-06 23:57:11 +0300

    Added some further instructions

--- solar_model.py
+++ solar_model.py
@@ -1,56 +1,54 @@
 # coding: utf-8
 # license: GPLv3
+import pygame
+
+RED = (255, 0, 0)
 
 gravitational_constant = 6.67408E-11
-"""Р“СЂР°РІРёС‚Р°С†РёРѕРЅРЅР°СЏ РїРѕСЃС‚РѕСЏРЅРЅР°СЏ РќСЊСЋС‚РѕРЅР° G"""
 
-
-def calculate_force(body, space_objects):
-    """Р’С‹С‡РёСЃР»СЏРµС‚ СЃРёР»Сѓ, РґРµР№СЃС‚РІСѓСЋС‰СѓСЋ РЅР° С‚РµР»Рѕ.
-
-    РџР°СЂР°РјРµС‚СЂС‹:
-
-    **body** вЂ” С‚РµР»Рѕ, РґР»СЏ РєРѕС‚РѕСЂРѕРіРѕ РЅСѓР¶РЅРѕ РІС‹С‡РёСЃР»РёС‚СЊ РґРµР№СЃС‚СѓСЋС‰СѓСЋ СЃРёР»Сѓ.
-    **space_objects** вЂ” СЃРїРёСЃРѕРє РѕР±СЉРµРєС‚РѕРІ, РєРѕС‚РѕСЂС‹Рµ РІРѕР·РґРµР№СЃС‚РІСѓСЋС‚ РЅР° С‚РµР»Рѕ.
+class Body:
+    """Тип данных, описывающий звезду.
+    Содержит массу, координаты, скорость звезды,
+    а также визуальный радиус звезды в пикселах и её цвет.
     """
+    def __init__(self, m=0, x=0, y=0, Vx=0, Vy=0, Fx=0, Fy=0, R=5, color= RED):# Ђттрибуты класса должны вводитьсЯ, а не быть заданными внутри класса. 
+        self.m = m
+        self.x = x
+        self.y = y
+        self.Vx = Vx
+        self.Vy = Vy
+        self.Fx = Fx
+        self.Fy = Fy
+        self.R = R
+        self.color = color# „обавить screen и type (звезда или солнце) как аттрибуты класса.
+        
+    def calculate_force(self, space_objects):
+        """Вычисляет силу, действующую на тело.
+        Параметры:
+        **space_objects** — список объектов, которые воздействуют на тело.
+        """
+        
+        self.Fx = self.Fy = 0
+        for obj in space_objects:
+            r = ((self.x - obj.x)**2 + (self.y - obj.y)**2)**0.5
+            self.Fx += gravitational_constant*self.m*obj.m*(obj.x - self.x)/(r*r*r)
+            self.Fy += gravitational_constant*self.m*obj.m*(obj.y - self.y)/(r*r*r)        
 
-    body.Fx = body.Fy = 0
-    for obj in space_objects:
-        if body == obj:
-            continue  # С‚РµР»Рѕ РЅРµ РґРµР№СЃС‚РІСѓРµС‚ РіСЂР°РІРёС‚Р°С†РёРѕРЅРЅРѕР№ СЃРёР»РѕР№ РЅР° СЃР°РјРѕ СЃРµР±СЏ!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: РЅСѓР¶РЅРѕ РІС‹РІРµСЃС‚Рё С„РѕСЂРјСѓР»Сѓ...
-        body.Fy += 2  # FIXME: РЅСѓР¶РЅРѕ РІС‹РІРµСЃС‚Рё С„РѕСЂРјСѓР»Сѓ...
-
-
-def move_space_object(body, dt):
-    """РџРµСЂРµРјРµС‰Р°РµС‚ С‚РµР»Рѕ РІ СЃРѕРѕС‚РІРµС‚СЃС‚РІРёРё СЃ РґРµР№СЃС‚РІСѓСЋС‰РµР№ РЅР° РЅРµРіРѕ СЃРёР»РѕР№.
-
-    РџР°СЂР°РјРµС‚СЂС‹:
-
-    **body** вЂ” С‚РµР»Рѕ, РєРѕС‚РѕСЂРѕРµ РЅСѓР¶РЅРѕ РїРµСЂРµРјРµСЃС‚РёС‚СЊ.
-    """
-
-    ax = body.Fx/body.m
-    body.x += 42  # FIXME: РЅРµ РїРѕРЅРёРјР°СЋ РєР°Рє РјРµРЅСЏС‚СЊ...
-    body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
-
-
-def recalculate_space_objects_positions(space_objects, dt):
-    """РџРµСЂРµСЃС‡РёС‚С‹РІР°РµС‚ РєРѕРѕСЂРґРёРЅР°С‚С‹ РѕР±СЉРµРєС‚РѕРІ.
-
-    РџР°СЂР°РјРµС‚СЂС‹:
-
-    **space_objects** вЂ” СЃРїРёСЃРѕРє РѕСЊСЉРµРєС‚РѕРІ, РґР»СЏ РєРѕС‚РѕСЂС‹С… РЅСѓР¶РЅРѕ РїРµСЂРµСЃС‡РёС‚Р°С‚СЊ РєРѕРѕСЂРґРёРЅР°С‚С‹.
-    **dt** вЂ” С€Р°Рі РїРѕ РІСЂРµРјРµРЅРё
-    """
-
-    for body in space_objects:
-        calculate_force(body, space_objects)
-    for body in space_objects:
-        move_space_object(body, dt)
-
-
-if __name__ == "__main__":
-    print("This module is not for direct call!")
+    def move_space_object(self, dt):
+        """Перемещает тело в соответствии с действующей на него силой.
+        Параметры:
+        **body** — тело, которое нужно переместить.
+        """
+            
+        ax = self.Fx/self.m
+        self.x += self.Vx*dt
+        self.Vx += ax*dt
+        ay = self.Fy/self.m
+        self.y += self.Vy*dt
+        self.Vy += ay*dt
+        
+    def draw_object(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.R)
+        
+    def check(self):
+    #Џроверка на наличие в пределах экрана.
